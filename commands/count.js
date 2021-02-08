7faf241572e646ee1cb0373223e854097ee7311a
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2021-02-08 17:15:32 +0200

    Cleaned up code

--- commands/count.js
+++ commands/count.js
@@ -5,44 +5,27 @@ module.exports = {
         let user = message.member;
         user = user.toString();
         let roleName = "";
-        
+
         let i = 0;
         args.forEach(element => {
             if (i == 0) roleName += element
             else {
                 roleName = roleName + " " + element
             }
-                i++;
+            i++;
         })
-        //let roleObject = message.guild.roles.cache.find(role => role.name === groupName)
         let roleId = message.guild.roles.cache.find(role => role.name === roleName);
-        //if (roleId === undefined) {
-          //  message.channel.send(user + " No such group exists.")
-            //return;
-        //}
 
         if (args.length == 0) {
             message.channel.send(user + " Count of users in the server: " + message.guild.memberCount);
         } else if (roleId) {
-                        //message.member.roles.cache.find(role => role.name === (i + " grupe"))
-                    let c = 0;
-                    //console.log(roleId);
-                    let count = message.guild.members.cache.filter(m => m.roles.cache.find(r => r.name === roleName));
-                    //let count = message.guild.members.cache.filter(m => m.roles.cache.find(r => r.name === roleName)).size;
-                    message.guild.members.cache.map(m =>{
-                        if(m.name == roleName) {
-                            c++
-                        }
-                    
-                    });
-                    //console.log(count);
-                    console.log(count)
-                    console.log("role count: " + c)
-                    //message.channel.send(user + " Count of users in " + roleName + ": "+count)
-                    
-        }       else {
-                    message.channel.send(user + " No such role found.");
-                     }
-        
+            //This part is obviously broken
+            //It used to work correctly is the past Discord.js versions.
+            //let count = message.guild.members.cache.filter(m => m.roles.cache.find(r => r.name === roleName)).size;
+            //message.channel.send(user + " Count of users in " + roleName + ": "+count)
+        } else {
+            message.channel.send(user + " No such role found.");
+        }
+
     }
 }
