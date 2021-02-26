3e882dda902067eda398c9c3cc9cc0fb6f44aa9c
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2021-02-26 13:25:47 +0200

    catch

--- commands/topmessage.js
+++ commands/topmessage.js
@@ -6,28 +6,33 @@ module.exports = {
 
         let user = message.member.toString();
         message.channel.send(user + " Counting reactions...");
-        const allMessages = await fetchAll.messages(message.channel, {
-            userOnly: true, // Only return messages by users
-        })
 
-        let topMessage;
-        let maxCount = 0;
+        try {
+            const allMessages = await fetchAll.messages(message.channel, {
+                userOnly: true, // Only return messages by users
+            })
 
-        allMessages.forEach(m => {
-            let reactions = m.reactions.cache.array();
-            reactions.forEach(r => {
-                if (r.count > maxCount) {
-                    maxCount = r.count;
-                    topMessage = m;
-                }
+            let topMessage;
+            let maxCount = 0;
+
+            allMessages.forEach(m => {
+                let reactions = m.reactions.cache.array();
+                reactions.forEach(r => {
+                    if (r.count > maxCount) {
+                        maxCount = r.count;
+                        topMessage = m;
+                    }
+
+                });
+
 
             });
-
-
-        });
-        message.channel.send(user + " This is the most reacted to message in this channel. With a top unique reaction count of: " + maxCount);
-        if (topMessage.pinned) topMessage.unpin({});
-        topMessage.pin({ reason: user + 'This is the most reacted to message in this channel' });
-        
+            message.channel.send(user + " This is the most reacted to message in this channel. With a top unique reaction count of: " + maxCount);
+            if (topMessage.pinned) topMessage.unpin({});
+            topMessage.pin({ reason: user + 'This is the most reacted to message in this channel' });
+        } catch (error) {
+            console.log(error.message);
+            message.channel.send(user + " There was an error in trying to count reactinos.");
+        }
     }
 }
